1520309d886eb3d7ffa092ea802852b3a47e7dee
yosachan <127594209+yosachan@users.noreply.github.com>
2023-04-09 01:33:52 +0900

    Add files via upload

---
+++ jk_kindergarten_009.py
@@ -0,0 +1,31 @@
+text1 = "123"
+print(int(text1))
+text2 = int(text1) + 456
+print(text2)
+text3 = "123.4"
+print(float(text3))
+num1 = 123
+print(str(num1))
+num2 = 123.4
+print(str(num2))
+height = 172
+print("自分の身長は、" + str(height)+ "cmです。")
+print(42)
+print(1, 2, 3)
+print("桃５つとみかん８つで", 100*5+40*8 ,"円です。")
+# string = input("入力してください。")
+# print("入力されたのは、", string, "です。")
+text1 = "lower Letter"
+Upper_text1 = text1.upper()
+print(Upper_text1)
+text2 = "The Shells she sells are sea-shells, I'm sure."
+print(text2.find("sea"))
+print("lowerletter".upper())
+data1=0.5
+print(data1.as_integer_ratio())
+mondai = input("文字列を入力してください。")
+komoji = mondai.lower()
+print(komoji)
+
+
+
